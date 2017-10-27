7f5378078936046f553b946035db9d2e84debd31
FlorineC <miniflorine@hotmail.fr>
2017-10-27 14:34:20 +0200

    Simple python code generator

---
+++ src/code_generator/code_generator_backend.py
@@ -0,0 +1,22 @@
+import sys, string
+
+class code_generator_backend:
+
+    def begin(self, tab="\t"):
+        self.code = []
+        self.tab = tab
+        self.level = 0
+
+    def end(self):
+        return string.join(self.code, "")
+
+    def write(self, string):
+        self.code.append(self.tab * self.level + string)
+
+    def indent(self):
+        self.level = self.level + 1
+
+    def dedent(self):
+        if self.level == 0:
+            raise SyntaxError, "internal error in code generator"
+        self.level = self.level - 1
---
+++ src/code_generator/fichier.py
@@ -0,0 +1,2 @@
+for i in range(1000):
+    print 'code generation is trivial'
