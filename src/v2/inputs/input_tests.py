84665a501fe22f8f86ab05ec656fd770f596b0c2
Denis Allard <allardden@gmail.com>
2017-11-27 14:00:07 +0100

    :white_check_mark: Adding tests.

--- src/v2/inputs/input_tests.py
+++ src/v2/inputs/input_tests.py
@@ -323,4 +323,104 @@ tests = {
         'result' : 3,
         'data' : [1,2,3,2,5,6,7,4,1,3,4,6,1,2,4,4]
     },
+    'sum_min_gorge' : {
+        'result' : 12,
+        'data' : [1,7,3,4,4,5,5,4,2,2,6,5,4,6,5,7]
+    },
+    'sum_min_increasing' : {
+        'result' : 12,
+        'data' : [4,3,5,5,2,1,1,3,3,4,6,6,3,1,3,3]
+    },
+    'sum_min_increasing_sequence' : {
+        'result' : 5,
+        'data' : [4,3,5,5,2,1,1,3,3,4,6,6,3,1,3,3]
+    },
+    'sum_min_inflexion' : {
+        'result' : 26,
+        'data' : [1,2,6,6,4,4,3,5,2,5,1,5,3,3,4,4]
+    },
+    'sum_min_strictly_decreasing_sequence' : {
+        'result' : 6,
+        'data' : [4,4,6,4,1,1,3,4,4,6,6,5,2,2,4,3]
+    },
+    'sum_min_strictly_increasing_sequence' : {
+        'result' : 5,
+        'data' : [4,3,5,5,2,1,1,2,3,4,6,6,3,1,2,3]
+    },
+    'sum_min_valley' : {
+        'result' : 10,
+        'data' : [1,3,7,4,3,6,6,5,3,3,2,6,5,5,5,7]
+    },
+    'sum_min_zigzag' : {
+        'result' : 3,
+        'data' : [4,1,3,1,4,6,1,5,5,2,7,2,3,1,6,1]
+    },
+    'sum_width_decreasing_sequence' : {
+        'result' : 9,
+        'data' : [3,4,2,2,5,6,6,4,4,3,1,1,4,6,4,4]
+    },
+    'sum_width_decreasing_terrace' : {
+        'result' : 4,
+        'data' : [6,4,4,4,5,2,2,1,3,3,5,4,4,3,3,3]
+    },
+    'sum_width_gorge' : {
+        'result' : 6,
+        'data' : [1,7,3,4,4,5,5,4,2,2,6,5,4,6,5,7]
+    },
+    'sum_width_increasing_sequence' : {
+        'result' : 9,
+        'data' : [4,3,5,5,2,1,1,3,3,4,6,6,3,1,3,3]
+    },
+    'sum_width_increasing_terrace' : {
+        'result' : 5,
+        'data' : [1,3,3,3,2,5,5,6,4,4,2,3,3,3,4,4]
+    },
+    'sum_width_inflexion' : {
+        'result' : 13,
+        'data' : [1,2,6,6,4,4,3,5,2,5,1,5,3,3,4,4]
+    },
+    'sum_width_peak' : {
+        'result' : 8,
+        'data' : [7,5,5,1,4,5,2,2,3,5,6,2,3,3,3,1]
+    },
+    'sum_width_plain' : {
+        'result' : 4,
+        'data' : [2,3,6,5,7,6,6,4,5,5,4,3,3,6,6,3]
+    },
+    'sum_width_plateau' : {
+        'result' : 7,
+        'data' : [1,3,3,5,5,5,5,2,4,4,4,3,3,1,5,5]
+    },
+    'sum_width_proper_plain' : {
+        'result' : 7,
+        'data' : [2,7,5,5,6,3,7,4,4,5,6,5,3,3,3,5]
+    },
+    'sum_width_proper_plateau' : {
+        'result' : 7,
+        'data' : [7,1,3,3,2,5,1,4,4,3,2,3,5,5,5,3]
+    },
+    'sum_width_steady_sequence' : {
+        'result' : 11,
+        'data' : [3,1,1,4,5,5,5,6,2,2,4,4,3,2,1,1]
+    },
+    'sum_width_strictly_decreasing_sequence' : {
+        'result' : 8,
+        'data' : [4,4,6,4,1,1,3,4,4,6,6,5,2,2,4,3]
+    },
+    'sum_width_strictly_increasing_sequence' : {
+        'result' : 10,
+        'data' : [4,3,5,5,2,1,1,2,3,4,6,6,3,1,2,3]
+    },
+    'sum_width_summit' : {
+        'result' : 6,
+        'data' : [7,1,5,4,4,3,3,4,6,6,2,3,4,2,3,1]
+    },
+    'sum_width_valley' : {
+        'result' : 9,
+        'data' : [1,3,7,4,3,6,6,5,3,3,2,6,5,5,5,7]
+    },
+    'sum_width_zigzag' : {
+        'result' : 11,
+        'data' : [4,1,3,1,4,6,1,5,5,2,7,2,3,1,6,1]
+    },
 }
