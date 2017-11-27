972d38012eb79d3dd9cf3f06d6ee1975db9410f1
Denis Allard <allardden@gmail.com>
2017-11-27 13:34:32 +0100

    :white_check_mark: Adding tests.

--- src/v2/inputs/input_tests.py
+++ src/v2/inputs/input_tests.py
@@ -263,4 +263,64 @@ tests = {
         'result' : 11,
         'data' : [7,6,5,6,5,4,1,4,7,5,4,2,5,4,3,3]
     },
+    'sum_max_decreasing' : {
+        'result' : 23,
+        'data' : [3,4,2,2,5,6,6,4,4,3,1,1,4,6,4,4]
+    },
+    'sum_max_decreasing_sequence' : {
+        'result' : 16,
+        'data' : [3,4,2,2,5,6,6,4,4,3,1,1,4,6,4,4]
+    },
+    'sum_max_dip_on_increasing_sequence' : {
+        'result' : 11,
+        'data' : [1,2,3,2,5,6,7,4,1,3,4,6,1,2,4,4]
+    },
+    'sum_max_increasing' : {
+        'result' : 21,
+        'data' : [4,3,5,5,2,1,1,3,3,4,6,6,3,1,3,3]
+    },
+    'sum_max_increasing_sequence' : {
+        'result' : 14,
+        'data' : [4,3,5,5,2,1,1,3,3,4,6,6,3,1,3,3]
+    },
+    'sum_max_inflexion' : {
+        'result' : 31,
+        'data' : [1,2,6,6,4,4,3,5,2,5,1,5,3,3,4,4]
+    },
+    'sum_max_peak' : {
+        'result' : 14,
+        'data' : [7,5,5,1,4,5,2,2,3,5,6,2,3,3,3,1]
+    },
+    'sum_max_strictly_decreasing_sequence' : {
+        'result' : 16,
+        'data' : [4,4,6,4,1,1,3,4,4,6,6,5,2,2,4,3]
+    },
+    'sum_max_strictly_increasing_sequence' : {
+        'result' : 14,
+        'data' : [4,3,5,5,2,1,1,2,3,4,6,6,3,1,2,3]
+    },
+    'sum_max_summit' : {
+        'result' : 12,
+        'data' : [7,1,5,4,4,3,3,4,6,6,2,3,4,2,3,1]
+    },
+    'sum_max_zigzag' : {
+        'result' : 16,
+        'data' : [4,1,3,1,4,6,1,5,5,2,7,2,3,1,6,1]
+    },
+    'sum_min_bump_on_decreasing_sequence' : {
+        'result' : 7,
+        'data' : [7,6,5,6,5,4,1,4,7,5,4,2,5,4,3,3]
+    },
+    'sum_min_decreasing' : {
+        'result' : 14,
+        'data' : [3,4,2,2,5,6,6,4,4,3,1,1,4,6,4,4]
+    },
+    'sum_min_decreasing_sequence' : {
+        'result' : 7,
+        'data' : [3,4,2,2,5,6,6,4,4,3,1,1,4,6,4,4]
+    },
+    'sum_min_dip_on_increasing_sequence' : {
+        'result' : 3,
+        'data' : [1,2,3,2,5,6,7,4,1,3,4,6,1,2,4,4]
+    },
 }
